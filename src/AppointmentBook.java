dfb4435803f82912c2632f0f1656b16e3a9d023f
VincentChen <vincentc273@nycstudents.net>
2025-10-30 13:20:38 -0400

    update 3

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -17,14 +17,13 @@ public class AppointmentBook {
                 if (block == duration) {
                     return i - duration + 1;
                 }
-            }
-            else block = 0;
+            } else block = 0;
         }
         return -1;
     }
 
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
-        for(int i = startPeriod; i <= endPeriod; i++) {
+        for (int i = startPeriod; i <= endPeriod; i++) {
             int freeBlock = findFreeBlock(i, duration);
             if (freeBlock > -1) {
                 reserveBlock(i, freeBlock, duration);
@@ -34,13 +33,13 @@ public class AppointmentBook {
         return false;
     }
 
-    public void printPeriod(int period) {
-        for (int i = 0; i < schedule[period - 1].length; i++)
-            System.out.println(i + " " + schedule[period - 1][i]);
-    }
+        public void printPeriod (int period){
+            for (int i = 0; i < schedule[period - 1].length; i++)
+                System.out.println(i + " " + schedule[period - 1][i]);
+        }
 
-    public void reserveBlock(int period, int startMinute, int duration) {
-        for(int i = startMinute; i < startMinute + duration; i++)
-            schedule[period - 1][i] = false;
+        public void reserveBlock ( int startMinute, int duration, int period){
+            for (int i = startMinute; i < startMinute + duration; i++)
+                schedule[period - 1][i] = false;
+        }
     }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,16 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Scanner;
+import java.sql.SQLOutput;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println(read());
+    public static void main(String args[]) {
         boolean[][] schedule = new boolean[8][60];
         AppointmentBook a = new AppointmentBook(schedule);
         for(int i = 10; i < 15; i++) schedule[1][i] = true;
         for(int i = 30; i < 45; i++) schedule[1][i] = true;
         for(int i = 50; i < 60; i++) schedule[1][i] = true;
         a.printPeriod(2);
-        System.out.println(a.findFreeBlock(2, 15));
-        System.out.println(a.findFreeBlock(2, 9));
-        System.out.println(a.findFreeBlock(2, 20));
+//        System.out.println(a.findFreeBlock(2, 15));
+//        System.out.println(a.findFreeBlock(2, 9));
+//        System.out.println(a.findFreeBlock(2, 20));
         schedule = new boolean[8][60];
         for(int i = 25; i < 30; i++) schedule[1][i] = true;
         for(int i = 0; i < 15; i++) schedule[2][i] = true;
@@ -22,41 +18,36 @@ public class Main {
         for(int i = 5; i < 30; i++) schedule[3][i] = true;
         for(int i = 44; i < 60; i++) schedule[3][i] = true;
         AppointmentBook b = new AppointmentBook(schedule);
-        int period = 2;
-        while(period < 5) {
-            System.out.println("Period: " + period);
-            b.printPeriod(period);
-            period++;
-        }
-        b.printPeriod(4);
-        System.out.println(b.makeAppointment(2, 4, 22));
-        b.printPeriod(4);
-    }
-    public static int read() throws FileNotFoundException {
-        int meetings = 0;
-        File f = new File("Schedules.txt");
-        Scanner s = new Scanner(f);
-        String schedule = "";
-        while (s.hasNext()) {
-            while (s.hasNextBoolean()) {
-                schedule += s.nextBoolean() + " ";
-            }
-            AppointmentBook a = new AppointmentBook(readSchedule(schedule));
-            schedule = "";
 
-            if (a.makeAppointment(s.nextInt(), s.nextInt(), s.nextInt()))
-                meetings++;
-        }
-        return meetings;
-    }
-    public static boolean[][] readSchedule(String lines) {
-        Scanner s = new Scanner(lines);
-        boolean[][] schedule = new boolean[8][60];
-        for (int i = 0; i < schedule.length; i++) {
-            for (int j = 0; j < schedule[i].length; j++) {
-                schedule[i][j] = s.nextBoolean();
-            }
-        }
-        return schedule;
+        System.out.println(b.makeAppointment(2, 4, 22));
+        //int period = 2;
+//        while(period < 5) {
+//            System.out.println("Period: " + period);
+//            b.printPeriod(period);
+//            period++;
+//        }
+//        for(int i = 0; i < 24; i++) schedule[2][i] = false;
+//        for(int i = 25; i < 29; i++) schedule[2][i] = true;
+//        for(int i = 30; i < 59; i++) schedule[2][i] = false;
+//        for(int i = 0; i < 14; i++) schedule[3][i] = true;
+//        for(int i = 15; i < 40; i++) schedule[3][i] = false;
+//        for(int i = 41; i < 59; i++) schedule[3][i] = true;
+//        for(int i = 0; i < 4; i++) schedule[4][i] = false;
+//        for(int i = 5; i < 29; i++) schedule[4][i] = true;
+//        for(int i = 30; i < 43; i++) schedule[4][i] = false;
+//        for(int i = 44; i < 59; i++) schedule[4][i] = true;
+//        a.printPeriod(3);
+//        for(int i = 0; i < 24; i++) schedule[2][i] = false;
+//        for(int i = 25; i < 29; i++) schedule[2][i] = true;
+//        for(int i = 30; i < 59; i++) schedule[2][i] = false;
+//        for(int i = 0; i < 2; i++) schedule[3][i] = false;
+//        for(int i = 3; i < 14; i++) schedule[3][i] = true;
+//        for(int i = 15; i < 40; i++) schedule[3][i] = false;
+//        for(int i = 41; i < 59; i++) schedule[3][i] = true;
+//        for(int i = 0; i < 26; i++) schedule[3][i] = false;
+//        for(int i = 27; i < 29; i++) schedule[3][i] = true;
+//        for(int i = 30; i < 43; i++) schedule[3][i] = false;
+//        for(int i = 44; i < 59; i++) schedule[3][i] = true;
+//        a.printPeriod(4);
     }
 }
