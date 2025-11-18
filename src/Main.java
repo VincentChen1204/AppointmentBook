be771a7b77a34dc3991adcc077e23b710291d831
VincentChen <vincentc273@nycstudents.net>
2025-11-18 12:49:23 -0500

    update 3

--- src/Main.java
+++ src/Main.java
@@ -20,34 +20,5 @@ public class Main {
         AppointmentBook b = new AppointmentBook(schedule);
 
         System.out.println(b.makeAppointment(2, 4, 22));
-        //int period = 2;
-//        while(period < 5) {
-//            System.out.println("Period: " + period);
-//            b.printPeriod(period);
-//            period++;
-//        }
-//        for(int i = 0; i < 24; i++) schedule[2][i] = false;
-//        for(int i = 25; i < 29; i++) schedule[2][i] = true;
-//        for(int i = 30; i < 59; i++) schedule[2][i] = false;
-//        for(int i = 0; i < 14; i++) schedule[3][i] = true;
-//        for(int i = 15; i < 40; i++) schedule[3][i] = false;
-//        for(int i = 41; i < 59; i++) schedule[3][i] = true;
-//        for(int i = 0; i < 4; i++) schedule[4][i] = false;
-//        for(int i = 5; i < 29; i++) schedule[4][i] = true;
-//        for(int i = 30; i < 43; i++) schedule[4][i] = false;
-//        for(int i = 44; i < 59; i++) schedule[4][i] = true;
-//        a.printPeriod(3);
-//        for(int i = 0; i < 24; i++) schedule[2][i] = false;
-//        for(int i = 25; i < 29; i++) schedule[2][i] = true;
-//        for(int i = 30; i < 59; i++) schedule[2][i] = false;
-//        for(int i = 0; i < 2; i++) schedule[3][i] = false;
-//        for(int i = 3; i < 14; i++) schedule[3][i] = true;
-//        for(int i = 15; i < 40; i++) schedule[3][i] = false;
-//        for(int i = 41; i < 59; i++) schedule[3][i] = true;
-//        for(int i = 0; i < 26; i++) schedule[3][i] = false;
-//        for(int i = 27; i < 29; i++) schedule[3][i] = true;
-//        for(int i = 30; i < 43; i++) schedule[3][i] = false;
-//        for(int i = 44; i < 59; i++) schedule[3][i] = true;
-//        a.printPeriod(4);
     }
 }
